4d8a02abd262dcc776ae512191ac596264418dc9
Gustavo Henrique <gustavohenriquecns.wk@gmail.com>
2023-11-09 16:10:20 -0800

    The First Part of interface was created and the img have been added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.gustavohenrique.interfacenetflix"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.gustavohenrique.interfacenetflix"
